7c3ce87c4d97aec526ed8d38f64233d3a2badb74
eh8 <echeng18@lawrenceville.org>
2018-03-23 15:26:16 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env nodejs
+var ip = "localhost";
+var port = 8080;
+
+var express = require('express');
+var parser = require("body-parser");
+var session = require("cookie-session");
+var router = require("./src/routes");
+var passport = require("passport");
+var app = express();
+require("./src/database");
+
+app.use("/",express.static("public"));
+app.use(parser.json());
+app.use(session({
+    name: 'session',
+    keys: ["lville1810","shreyc"],
+    maxAge: 60 * 60 * 1000
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(router);
+app.use(function(req, res) {
+    res.status(200).send('404: Page not Found', 404);
+});
+
+
+app.listen(port,ip,function() {
+    console.log('listening on '+ port);
+});
